8bb54bfec88f4e5c24d5d7922befd2725e210deb
thanapadhep-alt <thanapadhep@gmail.com>
2026-01-02 04:07:47 +0700

    Update service worker for caching and file names

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,36 @@
-const CACHE_NAME = 'sheetspro-v1';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './service-worker.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE_NAME = "sheets-pro-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(c => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      if (resp) return resp;
-      return fetch(event.request).then(r => {
-        return caches.open(CACHE_NAME).then(c => {
-          try { c.put(event.request, r.clone()); } catch(e){}
-          return r;
-        });
-      }).catch(()=> caches.match('./index.html'));
-    })
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
   );
 });
